aa0c9d519597792f82cc1a6d9761d6c463146612
Eddington Rowellis <eddingtonibi@gmail.com>
2022-12-04 18:48:31 +0300

    commit tree d92e2bfcd5fc64d0af063b1f20f513ba145f52be

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if (block > 42){
+        return (block - 42);
+    }   else if (block < 42){
+        return (42 - block);
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
